36a581e709296f4f8e7515a2a35a7b6798452b55
saicharandogiparthi <saicharandgp@gmail.com>
2024-01-07 16:12:16 +0530

    landing page vola finance

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,40 +17,98 @@
         }
     </style>
 </head>
+
 <body
     class=
     "antialiased
+    bg-gray-200
     bg-no-repeat"
     style="background-image: url('storage/VolaFinance/HeroCover.jpg');
     background-size: 100% 80vh"
 >
-<div class="flex">
-    <div class="flex-none w-1/4">
-        <div class="grid justify-items-end">
-            <span class="font-semibold capitalize">brandName</span>
-        </div>
-    </div>
-    <div class="flex-1 w-1/4">
-        <div class="grid grid-cols-4 gap-1 justify-items-end">
-            <div>Home</div>
-            <div>Product</div>
-            <div>Pricing</div>
-            <div>Content</div>
-        </div>
-    </div>
-    <div class="flex-1 w-1/2">
-        <div class="grid grid-cols-2 justify-items-center">
-            <div class="col-span-1/4">
-                Login
+<div class="h-full">
+    <div class="w-full container mt-2">
+        <div class="grid grid-cols-8 gap-4">
+            <div class="col-span-2 justify-self-end gap-2 font-bold text-xl p-3">
+                BrandName
             </div>
-            <div class="col-span-3/4">
-                Sign
-            </div>
-        </div>
-    </div>
-</div>
-<div>
 
+            <div class="col-span-2 ml-6 p-3">
+                <div class="grid grid-cols-4 gap-1">
+                    <div class="col-span-1 font-medium">Home</div>
+                    <div class="col-span-1 font-medium">Product</div>
+                    <div class="col-span-1 font-medium">Pricing</div>
+                    <div class="col-span-1 font-medium">Contact</div>
+                </div>
+            </div>
+
+            <div class="col-span-4">
+                <div class="grid grid-cols-2 gap-5 ">
+                    <div class="col-span-1 justify-self-end font-medium p-3">
+                        <a href="#">Login</a>
+                    </div>
+                    <div class="col-span-1 font-medium">
+                        <button class="col-span-1 font-medium bg-blue-500 text-white rounded-md px-3 py-3">
+                            Become a Member
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="pt-28 w-96 mx-auto grid grid-cols-1 gap-4 place-items-center place-content-center">
+            <div class="text-white font-bold text-4xl text-center">
+                Creating Beautifull & Usefull Solutions
+            </div>
+
+            <div class="text-white font-semibold text-center">
+                <p>We know how large projects will act, but things on a small scale do not act that way </p>
+            </div>
+
+            <div class="flex flex-wrap justify-between space-x-7">
+                <button class="rounded-full border px-6 py-3 bg-blue-500 text-white">
+                    Get Quote Now
+                </button>
+
+                <button class="rounded-full px-6 py-3 border text-white hover:bg-blue-500">
+                    Learn More
+                </button>
+            </div>
+        </div>
+
+        <div class="my-10 w-2/3 mx-auto grid grid-cols-12 gap-5 place-items-center place-content-center">
+            <div class="h-40 col-span-4 space-y-1 flex flex-col px-10 py-5 bg-white overflow-hidden shadow hover:shadow-md">
+                <div>
+                    <img alt="people" src="{{url('storage/VolaFinance/IcnPeopleColorIcon.png')}}">
+                </div>
+                <div>
+                    <p class="font-bold text-xl">Investment Trading</p>
+                </div>
+                <p class="text-wrap text-sm text-gray-600">the quick fox jumps over the lazy dog</p>
+            </div>
+
+            <div class="h-40 col-span-4 space-y-1 flex flex-col px-10 py-5 bg-white overflow-hidden shadow hover:shadow-md">
+                <div>
+                    <img alt="people" src="{{url('storage/VolaFinance/IcnPeopleColorIcon.png')}}">
+                </div>
+                <div>
+                    <p class="font-bold text-xl">Raising Funds</p>
+                </div>
+                <p class="text-wrap text-sm text-gray-600">the quick fox jumps over the lazy dog</p>
+            </div>
+
+            <div class="bg-sky-500 h-40 col-span-4 space-y-1 flex flex-col px-10 py-5 bg-white overflow-hidden shadow hover:shadow-md">
+                <div>
+                    <img alt="people" src="{{url('storage/VolaFinance/IcnPeopleIcon.png')}}">
+                </div>
+                <div>
+                    <p class="font-bold text-xl text-white">Financial Analysis</p>
+                </div>
+                <p class="text-wrap text-sm text-gray-600 text-slate-50">the quick fox jumps over the lazy dog</p>
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
